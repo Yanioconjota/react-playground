a890926a02282d8a15a737a15604d3b1d4c2014c
yanioconjota <janioisacura@gmail.com>
2024-11-22 19:11:57 -0300

    Added playground file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../public/vite.svg'
+import * as main from './playground/main';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // Use the imported code for debugging
+  const result = main.output();
+
+  console.log(result);
 
   return (
     <>
---
+++ src/playground/main.js
@@ -0,0 +1,7 @@
+function suma(a, b) {
+  return a + b;
+}
+
+export const output = () => {
+  return suma(5,5);
+}
